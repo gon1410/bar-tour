114133a671570381c70e4396d3e80f4761e64df3
gon1410 <ibgonzalo@gmail.com>
2016-11-07 17:18:10 -0300

    Lista de bares desde json

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,17 @@
 app = angular.module('app.controllers', []);
 
 
-app.controller('increBleListaDeBolichesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+app.controller('increBleListaDeBolichesCtrl', ['$scope', '$stateParams','$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+function ($scope, $stateParams, $http) {
+  $http.get('data/bares.json').success(function(response){
+    $scope.bares = response.bares;
+    console.log($scope.bares);
+  });
 }])
+
+
 
 app.controller('conectateCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
